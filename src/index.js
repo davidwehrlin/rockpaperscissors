4d272e5a5a2cdd2e0965156a3494ab289a827765
davidwehrlin <djwehrlin@gmail.com>
2021-08-07 19:04:39 -0600

    Added initial code for using tensorflow and handpose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import handpose from '@tensorflow-models/handpose';
+import '@tensorflow/tfjs-backend-webgl';
+
+//might need to enable hardware acceleration on chrome
 
 class App extends React.Component {
+
+  async makePrediction() {
+    const model = await handpose.load();
+    const predictions = await model.estimateHands(document.querySelector("#image"));
+
+    if (predictions.length > 0) {
+      for (let i = 0; i < predictions.length; i++) {
+        const keypoints = predictions[i].landmarks;
+  
+        // Log hand keypoints.
+        for (let i = 0; i < keypoints.length; i++) {
+          const [x, y, z] = keypoints[i];
+          console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+        }
+      }
+    }
+  }
+
   render() {
+    this.makePrediction();
     return (
       <div>
         Hello World!
